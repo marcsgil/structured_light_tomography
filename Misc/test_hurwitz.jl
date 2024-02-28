a899543c6b0846303760973177af0f27d74401f4
Marcos Gil <marcosgildeoliveira@gmail.com>
2024-02-28 11:01:17 -0300

    did some more tests

--- Misc/test_hurwitz.jl
+++ Misc/test_hurwitz.jl
@@ -1,18 +1,23 @@
-using BayesianTomography
+using BayesianTomography, LinearAlgebra
 
 angles = rand(8) * π
 
 hurwitz_parametrization(angles)
-BayesianTomography.hurwitz_parametrization2(angles)
-
-cos(angles[] / 2)
-##
 @benchmark hurwitz_parametrization($angles)
 @benchmark BayesianTomography.hurwitz_parametrization2($angles)
+##
+mean = 0.0
+N = 2
+nsamples = 10000
 
+for _ ∈ 1:nsamples
+    ψ₁ = random_angles(2(N - 1)) |> hurwitz_parametrization
+    ψ₂ = random_angles(2(N - 1)) |> hurwitz_parametrization
+    mean += abs(dot(ψ₁, [1 / √2, 1 / √2]))^2 / nsamples
+end
 
-
-
+mean
+##
 
 
 sc = sincos.(angles[1:4]) |> stack

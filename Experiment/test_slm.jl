f02aebff98ddde8d20f266ec4bc72188928d131e
Marcos Gil <marcosgildeoliveira@gmail.com>
2024-07-15 17:49:28 -0300

    minor changes

--- Experiment/test_slm.jl
+++ Experiment/test_slm.jl
@@ -1,6 +1,5 @@
 using SpatialLightModulator, StructuredLight
 
-
 slm = SLM()
 
 width = 15.36
@@ -12,12 +11,7 @@ x = LinRange(-width / 2, width / 2, resX)
 y = LinRange(-height / 2, height / 2, resY)
 
 incoming = hg(x, y, w=2.4)
-desired = lg(x, y, w=0.5, l=2)
+desired = hg(x, y, w=0.5, m=10,n=10)
 
-holo = generate_hologram(desired, incoming, x, y, 82, 20, 30)'
-update_hologram!(slm, holo)
-
-for _ ∈ range(1, 100)
-    update_hologram!(slm, rand(UInt8, slm.width, slm.height))
-    sleep(1 / slm.framerate)
-end
+holo = generate_hologram(desired, incoming, x, y, 82, 4, 5)'
+update_hologram(slm, holo)

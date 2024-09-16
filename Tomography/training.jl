bf940f4d405467d809dacb302bd1306ef1c071df
marcsgil <marcosgildeoliveira@gmail.com>
2024-09-16 19:09:20 -0300

    minor change

--- Tomography/training.jl
+++ Tomography/training.jl
@@ -13,19 +13,4 @@ model = get_model()
 
 ps, st = train(x, y, 200, model; device,
     model_saving_path="Tomography/TrainingLogs/best_model.jld2",
-    logging_path="Tomography/TrainingLogs/log.csv", patience=30);
-##
-"""x_exp = h5open("Data/Raw/positive_l.h5") do f
-    imresize(read(f["images_dim2"]), 64, 64)
-end |> device"""
-
-x_exp = h5open("Data/Raw/fixed_order_intense.h5") do f
-    imresize(f["images_order5"][:, :, 2, :], 64, 64)
-end |> device
-
-normalize_data!(x_exp, (1, 2))
-
-x_exp = reshape(x_exp, 64, 64, 1, 100)
-
-mean(model(x_exp, ps, st)[1], dims=2)
-#std(model(x_exp, ps, st)[1], dims=2)
+    logging_path="Tomography/TrainingLogs/log.csv", patience=50, opt=Lion());
